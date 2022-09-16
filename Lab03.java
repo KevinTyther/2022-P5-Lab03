9560dc351c365623b869f9a7ad54efb18b85b2f0
Kevin <kevin.tyther@vtsd.com>
2022-09-16 11:45:33 -0400

    Unfinished lab

---
+++ Lab03.java
@@ -0,0 +1,13 @@
+public class Lab03
+{
+
+    public static void main(String args[])
+    {
+        int seconds = 10000;
+        System.out.println("Starting seconds: " + seconds);
+        seconds = seconds/3600;
+        System.out.println("Hours: " + seconds);
+    }
+
+
+}
