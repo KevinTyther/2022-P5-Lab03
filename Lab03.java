d14b739f7d1ee899dd6ac90ef00a326bf0c89c7c
Kevin <kevin.tyther@vtsd.com>
2022-09-19 13:41:57 -0400

    Finished Lab 100 Point

--- Lab03.java
+++ Lab03.java
@@ -3,10 +3,22 @@ public class Lab03
 
     public static void main(String args[])
     {
-        int seconds = 10000;
-        System.out.println("Starting seconds: " + seconds);
-        seconds = seconds/3600;
-        System.out.println("Hours: " + seconds);
+        int milli = 10000123;
+        int hour;
+        int min;
+        int sec;
+        int mills;
+        System.out.println("Lab03, 100 Point Version\n");
+        System.out.println("Starting milli-seconds: " + milli);
+        hour = milli/3600000;
+        System.out.println("Hours: " + hour);
+        milli = milli%3600000;
+        min = milli/60000;
+        System.out.println("Minutes: " + min);
+        sec = milli%61;
+        System.out.println("Seconds: " + sec);
+        mills = milli%125;
+        System.out.println("milli Seconds: " + mills);
     }
 
 
